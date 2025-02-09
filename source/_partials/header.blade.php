e0bf9647bf3181db9cd1d6be38877f2672785743
Kresna Satya <kresnasatya72@gmail.com>
2025-02-09 13:01:25 +0800

    fix: remove hamburger menu

--- source/_partials/header.blade.php
+++ source/_partials/header.blade.php
@@ -1,18 +1,9 @@
-<header class="p-4 flex shadow bg-white items-center">
-    <button class="header__hamburger-btn p-2 md:hidden" aria-label="Open menu">
-        <img src="/assets/icons/list.svg" alt="" class="h-6 w-6">
-    </button>
+<header class="p-4 flex shadow bg-white items-center space-x-4">
     <a href="/" class="ml-4 md:ml-0">
         <img src="/assets/images/phpbali-logo.png" alt="PHPBali logo" width="50" height="50">
     </a>
     <nav id="sidebarMenu">
-        <div class="flex border-b border-solid border-gray-500 md:border-none p-6 -ml-2 -mt-1 md:hidden">
-            <!-- You can put your logo here ;) -->
-            <button class="nav__hide-btn p-2 ml-auto" aria-label="Close menu">
-              <img src="/assets/icons/times.svg" alt="" class="h-6 w-6">
-            </button>
-        </div>
-        <ul class="flex flex-col md:flex-row md:items-center">
+        <ul class="flex flex-row md:items-center space-x-4">
             <li>
                 <a href="/about" class="block md:inline font-bold md:mx-4 hover:bg-gray-200">
                     ABOUT
